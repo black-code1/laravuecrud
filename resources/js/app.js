a001a4abe319dbad45d2a85268a5b7dfe13f2530
black-code1 <legrandmahamat1@gmail.com>
2022-03-28 14:10:04 +0100

    setup vuejs app with router,components...

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
-require('./bootstrap');
+require("./bootstrap");
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
+
+import { createApp } from "vue";
+import router from "./router";
+import CompaniesIndex from "./components/companies/CompaniesIndex";
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+createApp({
+    components: { CompaniesIndex },
+})
+    .use(router)
+    .mount("#app");
